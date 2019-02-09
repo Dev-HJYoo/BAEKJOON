3c253064a3d20b57f86c217d6de89a085111ea39
cocopambag <jhdf1234@naver.com>
2019-02-09 23:23:59 +0900

    2019.2.9

--- 2812.cpp
+++ 2812.cpp
@@ -1,34 +1,59 @@
 /*
-	2019.2.4
-	일단 이번에 ignore 이라는 함수를 알게 되었고 개행문자를 안넣어서 계속 틀렸다고 나왔네...
-	 
+	2019.2.9
+	정훈이형 코드를 받아서 풀긴했는데... 내 원래코드는 왜 시간초과나는지 아직까진 잘 모르겠네...
+	전형적인 그리드 기법을 이용한 프로그래밍 문제였다.
 */
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
+#include <queue>
+#include <stdio.h>
+#define SIZE 500000
 using namespace std;
 
-int main(void){
-	 int n;
-	 
-	 cin>>n;
-	 
-	 for(int i = 0; i<n; i++){
-	 	string s;
-	 	cin>>s;
-	 	cin.ignore(80,'\n');
-		
-		int size = s.size();
-		int sum = 0;
-		int longs = 0;
-		for(int j = 0; j<size; j++){
-			if(s[j] == 'O'){
-				longs++;
-				sum += longs;
-			}
+
+int input[SIZE];
+
+
+struct compare { // pq 비교 구문, 
+	constexpr bool operator()(pair<int, int> const & a,
+		pair<int, int> const & b) const noexcept
+	{	
+		// 실제 값 비교
+		if (a.first > b.first)
+			return false;
+		else if (a.first == b.first) { // 실제 값이 같을경우 낮은 인덱스를 가진게 우선권을 가진다.
+			if (a.second < b.second)
+				return false;
 			else
-				longs = 0;
-			
+				return true;
 		}
-		cout<<sum<<"\n";
-	 }
+		else
+			return true;
+	}
+};
+
+
+int main(void) {
+	int n, k;
+	
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++)
+		scanf("%1d", &input[i]);
+	
+	priority_queue<pair<int, int>, vector<pair<int, int> >, compare> pq;
+	for (int i = 0; i < k; i++)
+		pq.push({ input[i], i });
+
+	int max_i = -1;
+
+	for (int i = k; i < n; i++) {
+		pq.push({ input[i],i });
+		
+		while (max_i > pq.top().second)
+			pq.pop();
+		
+		max_i = pq.top().second;
+		printf("%d", pq.top().first);
+		pq.pop();
+	}
 }
